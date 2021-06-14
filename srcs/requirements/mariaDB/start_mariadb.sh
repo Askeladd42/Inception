361460ad9be3ba41d6614b2952fb54f2f696705c
Askeladd42 <piema.lam@gmail.com>
2021-06-14 18:26:23 +0200

    containers are working, need configuration to work properly

--- srcs/requirements/mariaDB/start_mariadb.sh
+++ srcs/requirements/mariaDB/start_mariadb.sh
@@ -1,7 +1,9 @@
 #!bin/bash
- 
+
+service mysql start \
 mysql -u root -e "CREATE DATABASE wordpress_database" \
 mysql -u root -e "CREATE USER 'plam'@'Inception' IDENTIFIED BY 'oof'" \
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_database.* TO 'plam'@'Inception' IDENTIFIED BY 'oof'" \
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'plam'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -u root -e "FLUSH PRIVILEGES"
+mysql -u root -e "FLUSH PRIVILEGES" \
+mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /tmp/database.sql
