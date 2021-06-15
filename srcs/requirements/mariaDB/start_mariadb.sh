11a2da727064f54b43f75802facbebcaf9d48d2f
Askeladd42 <piema.lam@gmail.com>
2021-06-15 11:55:18 +0200

    adding ssl key + cert and modifications on the docekrfiles

--- srcs/requirements/mariaDB/start_mariadb.sh
+++ srcs/requirements/mariaDB/start_mariadb.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 
 service mysql start \
-mysql -u root -e "CREATE DATABASE wordpress_database" \
-mysql -u root -e "CREATE USER 'plam'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_database.* TO 'plam'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'plam'@'Inception' IDENTIFIED BY 'oof'" \
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE}" \
+mysql -u root -e "CREATE USER ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ROOT_USER}@'Inception' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD}" \
 mysql -u root -e "FLUSH PRIVILEGES" \
 mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /tmp/database.sql
+
+exec mysqld_safe --datadir="var/lib/mysql"
