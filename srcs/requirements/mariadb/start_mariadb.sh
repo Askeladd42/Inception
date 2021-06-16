7505d2e6410f5b94a3b5640ee68175365cf1d968
Askeladd42 <piema.lam@gmail.com>
2021-06-16 18:55:10 +0200

    containers are working, but still have problems on mariaDB & wp, working on it

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,5 +1,4 @@
 service mysql start \
-sleep infinity \
 mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE}" \
 mysql -u root -e "CREATE USER ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
