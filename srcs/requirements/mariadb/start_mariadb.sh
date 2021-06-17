dc84ff2e7ba0b418375d8fb1b8f13c83c3b2ad0f
Askeladd42 <piema.lam@gmail.com>
2021-06-17 12:49:31 +0200

    some functionsaddd to makefile, trying to run the mariadb + wp containers correctly

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-service mysql start \
+exec service mysql start \
 mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE}" \
 mysql -u root -e "CREATE USER ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
