1f8da115159f857c01b2959dd532ae788ee217a0
Askeladd42 <piema.lam@gmail.com>
2021-06-17 16:17:07 +0200

    some functions added to makefile, trying to run the mariadb + wp containers correctly, and .env dooesn't work

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 service mysql start \
-#mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE}" \
-#mysql -u root -e "CREATE USER ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
-#mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
-#mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ROOT_USER}@'Inception' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD}" \
+echo ${MYSQL_PASSWORD}
+echo env
+#mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE}"
+#mysql -u root -e "CREATE USER ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}"
+#mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}"
+#mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ROOT_USER}@'Inception' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD"
 #mysql -u root -e "FLUSH PRIVILEGES" \
 #mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /database.sql
+
+sleep infinity
 
 exec mysqld_safe --datadir="var/lib/mysql"
