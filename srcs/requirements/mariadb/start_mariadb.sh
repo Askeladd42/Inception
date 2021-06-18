397b24545f74767e5517fc64371b0018ec5839ff
Askeladd42 <piema.lam@gmail.com>
2021-06-18 12:36:23 +0200

    wp + mariadb containers still have problems, .env not working for the containers

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 service mysql start \
-#sleep infinity \
-mysql -u root -e "CREATE DATABASE 'Inception'" \
-mysql -u root -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -u root -e "GRANT ALL PRIVILEGES ON 'Inception' TO 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
-mysql -u root -e "FLUSH PRIVILEGES" \
+#mysql -u root -e "CREATE DATABASE 'Inception'" \
+#mysql -u root -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+#mysql -u root -e "GRANT ALL PRIVILEGES ON 'Inception' TO 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+#mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
+#mysql -u root -e "FLUSH PRIVILEGES" \
 #mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /database.sql \
 
 exec mysqld_safe --datadir="var/lib/mysql"
