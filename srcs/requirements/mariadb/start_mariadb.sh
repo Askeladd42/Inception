4533797ee6abd2e9e1378e7ac9ef23a33e853e0e
Askeladd42 <piema.lam@gmail.com>
2021-06-18 12:16:30 +0200

    wp + mariadb containers still have problems, .env not working for the containers

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 service mysql start \
-#mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE}" \
-#mysql -u root -e "CREATE USER ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
-#mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MYSQL_USER}@'Inception' IDENTIFIED BY ${MYSQL_PASSWORD}" \
-#mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ROOT_USER}@'Inception' IDENTIFIED BY ${MYSQL_ROOT_PASSWORD}" \
-#mysql -u root -e "FLUSH PRIVILEGES" \
-#mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /database.sql
+#sleep infinity \
+mysql -u root -e "CREATE DATABASE 'Inception'" \
+mysql -u root -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+mysql -u root -e "GRANT ALL PRIVILEGES ON 'Inception' TO 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
+mysql -u root -e "FLUSH PRIVILEGES" \
+#mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /database.sql \
 
 exec mysqld_safe --datadir="var/lib/mysql"
