39aa8dd57f514021a6fd6846e63f10106711f80b
Askeladd42 <piema.lam@gmail.com>
2021-06-19 11:40:23 +0200

    wordpres database to configure in mariaDB

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,11 +2,11 @@
 
 service mysql start \
 
-mysql -f "CREATE DATABASE 'Inception'" \
-mysql -f "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -f "GRANT ALL PRIVILEGES ON 'Inception' TO 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -f "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
-mysql -f "FLUSH PRIVILEGES" \
+mysql -u root -e "CREATE DATABASE 'Inception'" \
+mysql -u root -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+mysql -u root -e "GRANT ALL PRIVILEGES ON 'Inception' TO 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
+mysql -u root -e "FLUSH PRIVILEGES" \
 #mysql -u root -p ${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /database.sql \
 
 exec mysqld -u root --datadir="/var/lib/mysql/mysql"
