d2016a3e1fcb2c290551ae31cca00f1946264310
Askeladd42 <piema.lam@gmail.com>
2021-06-21 15:06:14 +0200

    wp + mariadb containers are restarting, working on the synthax errors

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -3,13 +3,14 @@
 service mysql start \
 
 mysql -e "CREATE DATABASE Inception" \
-mysql -e "CREATE USER corrector@Inception IDENTIFIED BY oof" \
-mysql -e "GRANT ALL PRIVILEGES ON 'Inception' TO corrector@Inception IDENTIFIED BY oof" \
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO boss@Inception IDENTIFIED BY bruh" \
+mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
+mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
+mysql -e "GRANT ALL PRIVILEGES ON 'Inception'.* TO 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
 mysql -e "FLUSH PRIVILEGES" \
 #mysql -u ${MYSQL_ROOT_USER} -p ${MYSQL_ROOT_PASSWORD} 
 #mysql -u root ${MYSQL_DATABASE} < /database.sql \
 
 exec mysqld -u root --datadir="/var/lib/mysql/mysql"
 
-# sql database + wp database missing, need to correct ASAP
+# need to correct the syntax for corrector user creation
