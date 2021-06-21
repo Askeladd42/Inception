d6d94d4973b933b3c47dce9a55057bd3d2209350
Askeladd42 <piema.lam@gmail.com>
2021-06-21 18:08:00 +0200

    making some tests, but have issues with the mariaDB & wp containers

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,14 +2,14 @@
 
 service mysql start \
 
-mysql -e "CREATE DATABASE Inception" \
-mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'" \
-mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'" \
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'" \					#not sure if '%' is necessary here
-#mysql -e "GRANT ALL PRIVILEGES ON 'Inception'.* TO 'corrector'@'Inception'" \
-mysql -e "FLUSH PRIVILEGES" \
+mysql -e "CREATE DATABASE Inception"
+mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'"
+mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'"					#not sure if '%' is necessary here
+#mysql -e "GRANT ALL PRIVILEGES ON 'Inception'.* TO 'corrector'@'Inception'"
+mysql -e "FLUSH PRIVILEGES"
 #mysql -u ${MYSQL_ROOT_USER} -p ${MYSQL_ROOT_PASSWORD} 
-#mysql -u root ${MYSQL_DATABASE} < /database.sql \
+#mysql -u root ${MYSQL_DATABASE} < /database.sql
 
 exec mysqld -u root --datadir="/var/lib/mysql/mysql"
 
