3cc789daf6161e9f4274831c6d1f07afbfd78ec5
Askeladd42 <piema.lam@gmail.com>
2021-06-22 18:52:20 +0200

    making some tests, but have issues with the mariaDB & wp containers

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
+#not sure if '%' is necessary here for boss user
 service mysql start \
 
 mysql -e "CREATE DATABASE Inception"
 mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'"
 mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'"					#not sure if '%' is necessary here
-#mysql -e "GRANT ALL PRIVILEGES ON 'Inception'.* TO 'corrector'@'Inception'"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'"
+mysql -e "GRANT ALL PRIVILEGES ON Inception.* TO 'corrector'@'Inception'"
 mysql -e "FLUSH PRIVILEGES"
 #mysql -u ${MYSQL_ROOT_USER} -p ${MYSQL_ROOT_PASSWORD} 
 #mysql -u root ${MYSQL_DATABASE} < /database.sql
