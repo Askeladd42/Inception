f83a619f8557b638278f2db917ae48901063fbf3
Askeladd42 <piema.lam@gmail.com>
2021-06-23 19:10:05 +0200

    socket issues with the mariaDB & wp containers

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #not sure if '%' is necessary here for boss user
-mysql start \
+service mysql start \
 
 mysql -e "CREATE DATABASE Inception"
 mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'"
