15ca83e0f93891d91a6a66fac93b1cfcf9131eb6
Askeladd42 <piema.lam@gmail.com>
2021-06-28 12:48:47 +0200

    mariadb is working, trying solve wp container issues

--- srcs/requirements/wordpress/wp_start.sh
+++ srcs/requirements/wordpress/wp_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-exec php -F start
+exec php-fpm7.3 -F
 
-# ERROR: unable to bind listening socket for address '/run/php/php7.3-fpm.sock': No such file or directory (2) to correct
+# [28-Jun-2021 10:46:27] ERROR: unable to bind listening socket for address '/run/php/php7.3-fpm.sock': No such file or directory (2)
+#[28-Jun-2021 10:46:27] ERROR: FPM initialization failed
