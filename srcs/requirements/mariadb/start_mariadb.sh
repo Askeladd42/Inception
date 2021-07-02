a93b07f8f703e00d83f8b04473e5891a6d87ac11
Askeladd42 <piema.lam@gmail.com>
2021-07-03 01:06:44 +0200

    my.cnf changed, but still got errors when starting mariaDB (socket issues), add some changes in others files, need more tests

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-#mysqld_install_db --no-defaults --basedir=/var/lib/mysql #conflict in the install, should check what happened that mess up the container
+mysql_install_db --defaults-file=/etc/mysql/my.cnf --basedir=/var/lib/mysql #conflict in the install, should check what happened that mess up the container
 
 service mysql start			#mysql fail to start, not creating the .sock for the db to work
 
@@ -10,6 +10,9 @@ mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'"
 mysql -e "GRANT ALL PRIVILEGES ON Inception.* TO 'corrector'@'Inception'"
 mysql -e "FLUSH PRIVILEGES"
+
 mysql Inception < /database.sql
+
+service mysql stop
 
 exec mysqld -u root --datadir="/var/lib/mysql"
