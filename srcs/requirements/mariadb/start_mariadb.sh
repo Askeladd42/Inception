b174f6363db1c737e9d167dd0c6f0e73cc26561d
Askeladd42 <piema.lam@gmail.com>
2021-07-03 16:09:39 +0200

    mariaDB works fine when launching the .sh in the container, but not with the docker-compose, add some changes in others files, need more tests

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,7 +2,7 @@
 
 service mysql status
 
-service mysql start			#mysql fail to start, not creating the .sock for the db to work
+service mysql start			#mysql fail to start, not creating the .sock for the db to work but only in the dockerfile execution
 sleep 100
 mysql -e "CREATE DATABASE Inception"
 mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'"
