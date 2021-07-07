65c0c9155c65ee41f2a01cbca5cd062a96809cd0
Askeladd42 <piema.lam@gmail.com>
2021-07-07 14:03:11 +0200

    added domain redirection & deletion, corrected some nginx.conf issues

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -3,10 +3,10 @@
 service mysql start
 sleep 10
 mysql -e "CREATE DATABASE ${MYSQL_DATABASE}"
-mysql -e "CREATE USER 'boss'@${MYSQL_DATABASE} IDENTIFIED BY 'bruh'"
-mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'"
-mysql -e "GRANT ALL PRIVILEGES ON Inception.* TO 'corrector'@'Inception'"
+mysql -e "CREATE USER ${WP_ADMIN}@${MYSQL_DATABASE} IDENTIFIED BY ${WP_ADMIN_PASSWORD}"
+mysql -e "CREATE USER ${WP_USER}@${MYSQL_DATABASE} IDENTIFIED BY ${WP_USER_PASSWORD}"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${WP_ADMIN}@'%' IDENTIFIED BY ${WP_ADMIN_PASSWORD}"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${WP_USER}@'${MYSQL_DATABASE}'"
 mysql -e "FLUSH PRIVILEGES"
 
 mysql ${MYSQL_DATABASE} < /database.sql
