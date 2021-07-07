ef73b50bcdd37b46e3bc6cdf962a0a34fccab02e
Askeladd42 <piema.lam@gmail.com>
2021-07-07 13:49:20 +0200

    added domain redirection & deletion, corrected some nginx.conf issues

--- srcs/requirements/mariadb/start_mariadb.sh
+++ srcs/requirements/mariadb/start_mariadb.sh
@@ -2,14 +2,14 @@
 
 service mysql start
 sleep 10
-mysql -e "CREATE DATABASE Inception"
-mysql -e "CREATE USER 'boss'@'Inception' IDENTIFIED BY 'bruh'"
+mysql -e "CREATE DATABASE ${MYSQL_DATABASE}"
+mysql -e "CREATE USER 'boss'@${MYSQL_DATABASE} IDENTIFIED BY 'bruh'"
 mysql -e "CREATE USER 'corrector'@'Inception' IDENTIFIED BY 'oof'"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'boss'@'%' IDENTIFIED BY 'bruh'"
 mysql -e "GRANT ALL PRIVILEGES ON Inception.* TO 'corrector'@'Inception'"
 mysql -e "FLUSH PRIVILEGES"
 
-mysql Inception < /database.sql
+mysql ${MYSQL_DATABASE} < /database.sql
 
 service mysql stop
 
